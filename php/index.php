00575caa0b0937ae7a44212ea72de5c21c8addca
Vasi <vasi2me@gmail.com>
2014-01-15 19:01:20 +0000

    Adding Openshift support files

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This makes our life easier when dealing with paths. Ever ything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Setup autoloading
+include 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
