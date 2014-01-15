c7ee0b5e7f2f9c6f9bf6ef529bc8092537475302
Vasi <vasi2me@gmail.com>
2014-01-15 07:35:10 +0000

    Changing the deploy script

--- .openshift/action_hooks/deploy.sh
+++ .openshift/action_hooks/deploy.sh
@@ -1,3 +1,8 @@
+// .openshift/action_hooks/deploy
 #!/bin/bash
 # .openshift/action_hooks/deploy
-( unset GIT_DIR ; cd $OPENSHIFT_REPO_DIR ; /usr/local/zend/bin/php composer.phar install )
+unset GIT_DIR
+cd $OPENSHIFT_REPO_DIR
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install --prefer-source --no-interaction  // the arguments is used to work around github api rate limits
+php composer.phar dump-autoload
